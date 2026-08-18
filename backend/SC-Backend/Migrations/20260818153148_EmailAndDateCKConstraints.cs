65044b01b86343c593751feba0908e94ca7bacec
MiljanMiki <miljan.dragovic@elfak.rs>
2026-08-18 17:32:48 +0200

    Dodate migracije za check constraints

---
+++ backend/SC-Backend/Migrations/20260818153148_EmailAndDateCKConstraints.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SC_Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class EmailAndDateCKConstraints : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_users_email",
+                table: "users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_games_game_settings_id",
+                table: "games");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_users_email",
+                table: "users",
+                sql: "email LIKE '%@%'");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_games_game_settings_id",
+                table: "games",
+                column: "game_settings_id");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_valid_end_time",
+                table: "games",
+                sql: "end_time IS NULL OR (end_time > start_time)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_users_email",
+                table: "users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_games_game_settings_id",
+                table: "games");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_valid_end_time",
+                table: "games");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_users_email",
+                table: "users",
+                sql: "email LIKE '%@%.%'");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_games_game_settings_id",
+                table: "games",
+                column: "game_settings_id",
+                unique: true);
+        }
+    }
+}
